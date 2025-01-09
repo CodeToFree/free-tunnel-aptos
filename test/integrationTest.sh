e656888f231c6658e248ef5e66a3277e1c002836
wyf-ACCEPT <43724111+wyf-ACCEPT@users.noreply.github.com>
2025-01-09 21:30:18 +0800

    [test] rewrite test

--- test/integrationTest.sh
+++ test/integrationTest.sh
@@ -1,24 +1,27 @@
-export ADMIN="0xa057ec0bff69d3e9a761337f8d6d9a742e93be316ba3062747af2734c96f2c15" # address #1
-export COIN_INFO="0x597f2b8b7bbc699436f7c1631f8188430e53b2f0ded6b7276f32896f3f4d1d6d"
-export COIN="$ADMIN::hello_rooch3::FSC"
+export ADMIN="0x0af854fcad035f4134636ff2d7fa22591f8ff2f264f354ac04e53da06e318529"       # address #1
+export COIN_ADMIN="0x3cf04c5602fbd9a8cb410174c3e46cf2c60d100431848d8f25375eef4f413480"  # address #2
+export COIN_INFO="0x58e9cd4a7f398a65bccfc556f36d3760dbf60d2d16cc3ae6943476182e2e5616"
+export COIN="$COIN_ADMIN::hello_rooch3::FSC"
 
 # 1. publish
+rooch account switch --address $ADMIN
 rooch move publish
 
 # 2. setupTreasuryCapManager
+rooch account switch --address $COIN_ADMIN
 rooch move run --function $ADMIN::minter_manager::setupTreasuryCapManager \
     --args object_id:$COIN_INFO --type-args $COIN
 
-# 2.1 return: TreasuryCapManager - 0x61707e7ffec111c1e3471d047bb9d08e82820844b158c00a64bb7f76ba686c16
-export TREASURY_CAP_MANAGER="0x61707e7ffec111c1e3471d047bb9d08e82820844b158c00a64bb7f76ba686c16"
+# 2.1 return: TreasuryCapManager - 0xa226fe20e853b328344f5e4787570c70851c24324f82f421328181e83e0a29d0
+export TREASURY_CAP_MANAGER="0xa226fe20e853b328344f5e4787570c70851c24324f82f421328181e83e0a29d0"
 
 # 3. issueMinterCap
-export MINTER="0xa1d54f2dbe95ccfd86181650e098d5bbbeb1ee12af16320745a51b87a8b4bfde"  # address #2
+export MINTER="0xef9201e82a49895312e4621ce73862700bdc8cc18b469906db712432808a6ae9"      # address #3
 rooch move run --function $ADMIN::minter_manager::issueMinterCap \
     --args object_id:$TREASURY_CAP_MANAGER --args address:$MINTER --type-args $COIN
 
-# 3.1 return: MinterCap - 0xf78d43ffed9bd10a0394663c1c4f08a5d8edf1851f230d85ad70d32c676a5501
-export MINTER_CAP="0x57099c0499a32310c41b022f92d180fd1ef8994f72e716db3cee3f82bf7fcd30"
+# 3.1 return: MinterCap - 0xb07046cc4e78b5dddbd78a3e302700664d44615a6fe60ce4a1bf25fecf431f85
+export MINTER_CAP="0xb07046cc4e78b5dddbd78a3e302700664d44615a6fe60ce4a1bf25fecf431f85"
 
 # 4. mint
 export RECIPIENT="0xb072a8901831f11fb096aa53bbcebc9d5bf7d503d1ac52c911db7a4bcf3c51e2"
@@ -40,7 +43,7 @@ rooch move run --function $ADMIN::minter_manager::burnFromSigner \
 rooch account balance --address $MINTER
 
 # 6. revokeMinterCap
-rooch account switch --address $ADMIN
+rooch account switch --address $COIN_ADMIN
 rooch move run --function $ADMIN::minter_manager::revokeMinterCap \
     --args object_id:$TREASURY_CAP_MANAGER --args object_id:$MINTER_CAP \
     --type-args $COIN
@@ -56,6 +59,19 @@ rooch move run --function $ADMIN::minter_manager::mint \
 ## cannot destroy TreasuryCapManager, because it's a shared object
 
 # 9. add token
+rooch account switch --address $ADMIN
 rooch move run --function $ADMIN::atomic_mint::addToken \
-    --args 36u8 --args object_id:$COIN_INFO \
-    --type-args $COIN
+    --args 36u8 --args 18u8 --type-args $COIN
+
+# 10. transferMinterCap
+rooch account switch --address $COIN_ADMIN
+rooch move run --function $ADMIN::minter_manager::issueMinterCap \
+    --args object_id:$TREASURY_CAP_MANAGER --args address:$COIN_ADMIN --type-args $COIN
+
+export MINTER_CAP="0x8b9a8db1ddf9413bc4bea733b8bea324bb43848ae74e449dc9a0066bf0283708"
+rooch move run --function $ADMIN::atomic_mint::transferMinterCap \
+    --args 36u8 --args object_id:$MINTER_CAP --type-args $COIN
+
+# 11. removeToken
+rooch account switch --address $ADMIN
+rooch move run --function $ADMIN::atomic_mint::removeToken --args 36u8 --type-args $COIN
